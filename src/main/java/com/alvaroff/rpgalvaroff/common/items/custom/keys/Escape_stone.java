dd1ba33dd05851d0434e2bdf62b14af3ee6073f1
varo12ff <varo12ff@gmail.com>
2024-02-29 19:18:17 +0100

    Introduccion del item piedra de escape.

---
+++ src/main/java/com/alvaroff/rpgalvaroff/common/items/custom/keys/Escape_stone.java
@@ -0,0 +1,9 @@
+package com.alvaroff.rpgalvaroff.common.items.custom.keys;
+
+import net.minecraft.world.item.Item;
+
+public class Escape_stone extends Item {
+    public Escape_stone(Properties p) {
+        super(p);
+    }
+}
