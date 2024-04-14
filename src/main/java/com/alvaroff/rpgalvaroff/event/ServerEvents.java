86187903347e828a093f8604caed9b8c04bc8998
varo12ff <varo12ff@gmail.com>
2024-04-14 19:51:01 +0200

    Sistema de experiencia implementado

--- src/main/java/com/alvaroff/rpgalvaroff/event/ServerEvents.java
+++ src/main/java/com/alvaroff/rpgalvaroff/event/ServerEvents.java
@@ -10,18 +10,24 @@ import com.alvaroff.rpgalvaroff.capabilities.dungeonState.DungeonState;
 import com.alvaroff.rpgalvaroff.capabilities.dungeonState.DungeonStateProvider;
 import com.sun.jna.platform.unix.X11;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.UUID;
 
 @Mod.EventBusSubscriber(modid = RPGalvaroff.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ServerEvents {
@@ -54,6 +60,18 @@ public class ServerEvents {
         if(!player.getLevel().isClientSide()) {
             player.getAttribute(Attributes.MAX_HEALTH).setBaseValue(player.getCapability(PlayerStatsProvider.PLAYER_STATS).orElse(new PlayerStats()).getHealth());
             PlayerUtils.changeAttributes(player);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onMobDeath(LivingDeathEvent event){
+        if(event.getSource().getEntity() instanceof Player){
+
+            Player player = (Player) event.getSource().getEntity();
+            LivingEntity mob = event.getEntityLiving();
+            int newXp = (int)mob.getMaxHealth() / 2;
+
+            player.getCapability(PlayerStatsProvider.PLAYER_STATS).orElse(new PlayerStats()).addXp(newXp);
         }
     }
 
