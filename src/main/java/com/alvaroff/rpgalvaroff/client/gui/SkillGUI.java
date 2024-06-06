12536de6a52552a64fea0f2dbabb6612df1b3063
varo12ff <varo12ff@gmail.com>
2024-06-06 20:13:12 +0200

    SkillGUI completa y funcional + fixbugs

--- src/main/java/com/alvaroff/rpgalvaroff/client/gui/SkillGUI.java
+++ src/main/java/com/alvaroff/rpgalvaroff/client/gui/SkillGUI.java
@@ -10,10 +10,8 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
 
 import java.util.ArrayList;
 
